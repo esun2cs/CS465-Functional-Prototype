b7f63f7d97b8eaa88bcdd92d65f135e61678b79c
jennifer <jlope262@illinois.edu>
2025-11-12 20:46:13 -0600

    latest changes added bav bar and other fixes

--- CS465Prototype/app/src/main/java/com/example/cs465prototype/BusinessProfileActivity.java
+++ CS465Prototype/app/src/main/java/com/example/cs465prototype/BusinessProfileActivity.java
@@ -7,6 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class BusinessProfileActivity extends AppCompatActivity {
 
@@ -15,6 +22,21 @@ public class BusinessProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_business_profile);
+        //views
+        Button backBtn = findViewById(R.id.back);
+        ImageButton starBtn = findViewById(R.id.star);
+        ImageView mainImg = findViewById(R.id.bussinesImg);
+
+        //back button
+        backBtn.setOnClickListener(v -> {
+            if (getParentActivityIntent() != null || getCallingActivity() != null) {
+                finish();
+            } else {
+                Intent i = new Intent(BusinessProfileActivity.this, searchActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

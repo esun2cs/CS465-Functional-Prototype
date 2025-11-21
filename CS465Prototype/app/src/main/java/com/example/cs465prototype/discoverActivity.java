243eb8acdadd800485616d42b3fa2d16bdb95711
Benhao Lu <laoluproductions@outlook.com>
2025-11-21 11:40:26 -0600

    Working version of Favorites section

--- CS465Prototype/app/src/main/java/com/example/cs465prototype/discoverActivity.java
+++ CS465Prototype/app/src/main/java/com/example/cs465prototype/discoverActivity.java
@@ -3,14 +3,12 @@ package com.example.cs465prototype;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -38,119 +36,69 @@ public class discoverActivity extends AppCompatActivity {
             dm.loadFromJson(this);
         }
 
-
         LayoutInflater inflater = LayoutInflater.from(this);
 
         for (Business b : dm.allBusinesses) {
             if (b.isNew) {
-
-                // Inflate card
                 View card = inflater.inflate(R.layout.business_card, cardContainer, false);
 
-                // Set name
-                TextView name = card.findViewById(R.id.business_name);
-                name.setText(b.name);
+                ((TextView) card.findViewById(R.id.business_name)).setText(b.name);
+                ((TextView) card.findViewById(R.id.business_added)).setText("Added recently");
+                ((TextView) card.findViewById(R.id.business_category)).setText(b.category);
+                ((TextView) card.findViewById(R.id.business_description)).setText(b.description);
 
-                // Example: "Added 12 days ago"
-                TextView added = card.findViewById(R.id.business_added);
-                added.setText("Added recently");
-
-                // Set category
-                TextView category = card.findViewById(R.id.business_category);
-                category.setText(b.category);
-
-                // Set description
-                TextView desc = card.findViewById(R.id.business_description);
-                desc.setText(b.description);
-
-                // Set image (handles PNG/JPG + normalizes the name)
                 ImageView img = card.findViewById(R.id.business_photo);
-
-                String imageName = b.photo
-                        .toLowerCase()
-                        .replace(".jpg", "")
-                        .replace(".jpeg", "")
-                        .replace(".png", "")
-                        .replace("-", "_")
-                        .replace(" ", "_");
-
+                String imageName = b.photo.toLowerCase().replace(".jpg", "").replace(".jpeg", "").replace(".png", "").replace("-", "_").replace(" ", "_");
                 int imgRes = getResources().getIdentifier(imageName, "drawable", getPackageName());
-
                 if (imgRes != 0) {
                     img.setImageResource(imgRes);
                 } else {
-                    img.setImageResource(R.drawable.ic_launcher_background); // TEMP FALLBACK
+                    img.setImageResource(R.drawable.ic_launcher_background);
                 }
 
-                // Add click handler
                 card.setOnClickListener(v -> {
                     Intent i = new Intent(this, BusinessProfileActivity.class);
                     i.putExtra("business_id", b.id);
                     startActivity(i);
                 });
 
-                // Add card to container
+                ImageView star = card.findViewById(R.id.business_favorite_star);
+                updateStarIcon(star, b.favorited);
+
+                star.setOnClickListener(v -> {
+                    b.favorited = !b.favorited;
+                    updateStarIcon(star, b.favorited);
+                });
+
                 cardContainer.addView(card);
             }
         }
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            //        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.nav_home) {
-                    // do something
-                    Intent i = new Intent(discoverActivity.this, MainActivity.class);
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                    startActivity(i);
-                    return true;
-                } else if (id == R.id.nav_discover) {
-                    launchDiscover(null);
-                    return true;
-                } else if (id == R.id.nav_search) {
-                    launchSearch(null);
-                    return true;
-                } else if (id == R.id.nav_favorites) {
-                    launchFavorites(null);
-                    return true;
-                }
-                return false;
+        bottomNavigationView.setSelectedItemId(R.id.nav_discover);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            } else if (id == R.id.nav_discover) {
+                return true;
+            } else if (id == R.id.nav_search) {
+                startActivity(new Intent(this, searchActivity.class));
+                return true;
+            } else if (id == R.id.nav_favorites) {
+                startActivity(new Intent(this, favoritesActivity.class));
+                return true;
             }
+            return false;
         });
     }
 
-    public void launchSearch(View v) {
-        // launch Search page
-
-        // Instantiate a new object of type intent, assigned to variable i
-        Intent i = new Intent(this, searchActivity.class);
-        startActivity(i);
-
-    }
-
-    public void launchDiscover(View v) {
-        // launch Search page
-
-        // Instantiate a new object of type intent, assigned to variable i
-        Intent i = new Intent(this, discoverActivity.class);
-        startActivity(i);
-
-    }
-
-    public void launchFavorites(View v) {
-        // launch Search page
-
-        // Instantiate a new object of type intent, assigned to variable i
-        Intent i = new Intent(this, favoritesActivity.class);
-        startActivity(i);
-
-    }
-
-    public void launchBusinessProfile(View v) {
-        // launch business page
-        Intent i = new Intent(this, BusinessProfileActivity.class);
-        startActivity(i);
+    private void updateStarIcon(ImageView star, boolean isFav) {
+        if (isFav) {
+            star.setImageResource(android.R.drawable.btn_star_big_on);
+        } else {
+            star.setImageResource(android.R.drawable.btn_star_big_off);
+        }
     }
 }

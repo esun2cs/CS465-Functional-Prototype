40e89d7ed7f829965ae0879f099b704a0f842fa4
esun2cs <esun3247@gmail.com>
2025-12-04 03:34:41 -0600

    fixed search issue, fixed favorites nav bar labels

--- CS465Prototype/app/src/main/java/com/example/cs465prototype/SearchAdapter.java
+++ CS465Prototype/app/src/main/java/com/example/cs465prototype/SearchAdapter.java
@@ -78,11 +78,10 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
         return businesses.size();
     }
 
-//    // Optional: called from filterActivity result – right now just refreshes
-//    public void applyFilters(String location, int maxPrice, String tag) {
-//        // If you later want advanced filters, you can filter `businesses` here.
-//        notifyDataSetChanged();
-//    }
+    public void applyFilters(String location, int maxPrice, String tag) {
+        // If you later want advanced filters, you can filter `businesses` here.
+        notifyDataSetChanged();
+    }
 
     private void updateStarIcon(ImageView starView, boolean isFav) {
         if (isFav) {
